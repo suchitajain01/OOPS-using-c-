4d3578af71218e277160b3a15c91cbd252fead69
Suchita Jain <jsuchita923@gmail.com>
2026-08-29 16:45:01 +0530

    templates

---
+++ templates2.cpp
@@ -0,0 +1,46 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+/*
+
+template<class T1, class T2>
+void display(T1 a, T2 b)
+{
+  // cout<<"The sum is :"<<a + b; 
+  return a +b;
+}
+
+
+int main()
+{
+
+//display(10,20);
+//display(4.5,6.7);
+ //string name = display("A","B");
+ //cout<<name;
+
+int r1 = display(10,20);
+cout<<r1<<endl;
+
+double r2  = display(4.5,6.5);
+cout<<r2<<endl;;
+
+
+    return 0;
+}
+    */
+
+   /*
+    template<typename T,typename....args   >
+    void display(T first,args rest )
+    {
+        cout<<first<<endl;
+        display(rest...)
+    } 
+    
+    int main()
+    {
+      display(10,"")
+        return 0;
+    }
+*/
